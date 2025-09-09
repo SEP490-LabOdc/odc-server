5abcae583305a3290ab301410afb6de704f1d186
21anhn <ltnanh21@gmail.com>
2025-09-09 23:25:21 +0700

    feat: update ApiGatewayApplicationTests file

--- infrastructure/api-gateway/src/test/java/com/odc/apigateway/ApiGatewayApplicationTests.java
+++ infrastructure/api-gateway/src/test/java/com/odc/apigateway/ApiGatewayApplicationTests.java
@@ -3,7 +3,14 @@ package com.odc.apigateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+        properties = {
+                "spring.main.web-application-type=reactive",
+                "eureka.client.enabled=false",
+                "spring.cloud.config.enabled=false"
+        }
+)
+
 class ApiGatewayApplicationTests {
 
     @Test
