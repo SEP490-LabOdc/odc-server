2a3d0ffd66681b721d0031d51e4a0c4fbb87422b
21anhn <ltnanh21@gmail.com>
2025-09-13 09:35:06 +0700

    feat: add display standard name method for role enum

--- shared/odc-common/src/main/java/com/odc/common/constant/Role.java
+++ shared/odc-common/src/main/java/com/odc/common/constant/Role.java
@@ -1,4 +1,17 @@
 package com.odc.common.constant;
 
 public enum Role {
+    ADMIN("Admin"),
+    USER("User");
+
+    private final String displayName;
+
+    Role(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @Override
+    public String toString() {
+        return this.displayName;
+    }
 }
