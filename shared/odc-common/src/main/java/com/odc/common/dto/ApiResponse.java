7063367c706c535c536ce794a940829eeba2df4d
21anhn <ltnanh21@gmail.com>
2025-09-27 16:50:16 +0700

    feat: optimize imports for common module

--- shared/odc-common/src/main/java/com/odc/common/dto/ApiResponse.java
+++ shared/odc-common/src/main/java/com/odc/common/dto/ApiResponse.java
@@ -6,13 +6,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ApiResponse<T>  {
+public class ApiResponse<T> {
     private boolean success;
     private String message;
     private T data;
