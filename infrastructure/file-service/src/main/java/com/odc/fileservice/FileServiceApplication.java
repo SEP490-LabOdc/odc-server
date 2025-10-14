7d38e614d22397f29252d6b913279d92c1871859
DucSE170473 <ducha.bidv@gmail.com>
2025-10-14 21:54:12 +0700

    feat: implement file service with S3 integration and H2 database support

--- infrastructure/file-service/src/main/java/com/odc/fileservice/FileServiceApplication.java
+++ infrastructure/file-service/src/main/java/com/odc/fileservice/FileServiceApplication.java
@@ -2,15 +2,8 @@ package com.odc.fileservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(
-        basePackages = {
-                "com.odc.fileservice",
-                "com.odc.common.config",
-        }
-)
 public class FileServiceApplication {
 
     public static void main(String[] args) {
