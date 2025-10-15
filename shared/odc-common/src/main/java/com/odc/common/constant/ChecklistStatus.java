45f9e1532e81bdb899823e28a3d43a4665c73726
21anhn <ltnanh21@gmail.com>
2025-10-15 18:05:22 +0700

    refactor: remove displayName field in enums

--- shared/odc-common/src/main/java/com/odc/common/constant/ChecklistStatus.java
+++ shared/odc-common/src/main/java/com/odc/common/constant/ChecklistStatus.java
@@ -1,19 +1,8 @@
 package com.odc.common.constant;
 
 public enum ChecklistStatus {
-    PENDING("Pending"),
-    IN_PROGRESS("In Progress"),
-    COMPLETED("Completed"),
-    INVALID("Invalid");
-
-    private final String displayName;
-
-    ChecklistStatus(String displayName) {
-        this.displayName = displayName;
-    }
-
-    @Override
-    public String toString() {
-        return this.displayName;
-    }
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED,
+    INVALID
 }
--- shared/odc-common/src/main/java/com/odc/common/constant/Gender.java
+++ shared/odc-common/src/main/java/com/odc/common/constant/Gender.java
@@ -1,18 +1,7 @@
 package com.odc.common.constant;
 
 public enum Gender {
-    MALE("Male"),
-    FEMALE("Female"),
-    OTHER("Other");
-
-    private final String displayName;
-
-    Gender(String displayName) {
-        this.displayName = displayName;
-    }
-
-    @Override
-    public String toString() {
-        return this.displayName;
-    }
+    MALE,
+    FEMALE,
+    OTHER
 }
