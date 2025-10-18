8968b4429086cf2bd9875646e4c43ba63525e862
21anhn <ltnanh21@gmail.com>
2025-10-18 09:18:24 +0700

    feat(core): Add event handlers and gRPC service

---
+++ domain-services/user-service/src/main/java/com/odc/userservice/grpc/UserServiceGrpcImpl.java
@@ -0,0 +1,57 @@
+package com.odc.userservice.grpc;
+
+import com.odc.common.constant.Status;
+import com.odc.company.v1.RecipientResolverServiceGrpc;
+import com.odc.company.v1.ResolveRecipientsRequest;
+import com.odc.company.v1.ResolveRecipientsResponse;
+import com.odc.notification.v1.Target;
+import com.odc.userservice.repository.UserRepository;
+import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+import java.util.List;
+
+@GrpcService
+@RequiredArgsConstructor
+@Slf4j
+public class UserServiceGrpcImpl extends RecipientResolverServiceGrpc.RecipientResolverServiceImplBase {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public void resolveRecipients(ResolveRecipientsRequest request, StreamObserver<ResolveRecipientsResponse> responseObserver) {
+        Target target = request.getTarget();
+
+        ResolveRecipientsResponse.Builder responseBuilder = ResolveRecipientsResponse.newBuilder();
+
+        switch (target.getTargetTypeCase()) {
+            case ALL:
+                boolean includeInactive = target.getAll().getIncludeInactive();
+                if (includeInactive)
+                    responseBuilder.addAllUserIds(userRepository.findAllUserIds().stream().map(String::valueOf).toList());
+                else
+                    responseBuilder.addAllUserIds(userRepository.findAllActiveUserIds(Status.ACTIVE.toString()).stream().map(String::valueOf).toList());
+                break;
+
+            case ROLE:
+                List<String> roles = target.getRole().getRolesList();
+                responseBuilder.addAllUserIds(userRepository.findUserIdsByRoles(roles).stream().map(String::valueOf).toList());
+                break;
+
+            case USER:
+                List<String> userIds = target.getUser().getUserIdsList();
+                responseBuilder.addAllUserIds(userIds);
+                break;
+
+            case TARGETTYPE_NOT_SET:
+            default:
+                break;
+        }
+
+        log.info("response data: {}", responseBuilder.build());
+        responseObserver.onNext(responseBuilder.build());
+        responseObserver.onCompleted();
+    }
+}
