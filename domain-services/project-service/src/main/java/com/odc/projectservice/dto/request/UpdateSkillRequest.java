bd1752545191e42a93d4b960bcc7c3757f7303c2
DucSE170473 <ducha.bidv@gmail.com>
2025-10-24 15:12:14 +0700

    feat(skill): Add SkillController, SkillService, SkillServiceImpl, and DTO classes for CreateSkillRequest, UpdateSkillRequest, and SkillResponse

---
+++ domain-services/project-service/src/main/java/com/odc/projectservice/dto/request/UpdateSkillRequest.java
@@ -0,0 +1,13 @@
+package com.odc.projectservice.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateSkillRequest {
+    @NotBlank(message = "Tên kỹ năng không được để trống")
+    private String name;
+    private String description;
+}
---
+++ domain-services/project-service/src/main/java/com/odc/projectservice/dto/response/SkillResponse.java
@@ -0,0 +1,19 @@
+package com.odc.projectservice.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+public class SkillResponse {
+    private UUID id;
+    private String name;
+    private String description;
+    private LocalDateTime createdAt;
+    private  LocalDateTime updatedAt;
+}
