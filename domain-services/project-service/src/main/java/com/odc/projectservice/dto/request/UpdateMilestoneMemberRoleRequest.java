8c88728f7ace2a71fcb8062843ac6b54fdda776c
DucSE170473 <ducha.bidv@gmail.com>
2025-12-08 16:29:17 +0700

    feat(milestone): add isLeader property to update milestone member role request

--- domain-services/project-service/src/main/java/com/odc/projectservice/dto/request/UpdateMilestoneMemberRoleRequest.java
+++ domain-services/project-service/src/main/java/com/odc/projectservice/dto/request/UpdateMilestoneMemberRoleRequest.java
@@ -1,8 +1,10 @@
 package com.odc.projectservice.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class UpdateMilestoneMemberRoleRequest {
+    @JsonProperty("isLeader")
     private boolean isLeader;
 }
