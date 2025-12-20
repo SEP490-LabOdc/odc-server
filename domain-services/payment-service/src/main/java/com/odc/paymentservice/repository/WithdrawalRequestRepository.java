3cb1d56b7d4b2f013aa7a94c8b47f1dc80ac3d53
21anhn <ltnanh21@gmail.com>
2025-12-20 16:12:13 +0700

    update: refactor sql query

--- domain-services/payment-service/src/main/java/com/odc/paymentservice/repository/WithdrawalRequestRepository.java
+++ domain-services/payment-service/src/main/java/com/odc/paymentservice/repository/WithdrawalRequestRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -15,10 +16,16 @@ public interface WithdrawalRequestRepository extends JpaRepository<WithdrawalReq
     Page<WithdrawalRequest> findByStatus(String status, Pageable pageable);
 
     @Query("""
-            select wr from WithdrawalRequest wr
-            where (:status is null or wr.status = :status)
-              and (:from is null or wr.createdAt >= :from)
-              and (:to is null or wr.createdAt <= :to)
+                select wr from WithdrawalRequest wr
+                where (:status is null or wr.status = :status)
+                  and (cast(:from as timestamp) is null or wr.createdAt >= :from)
+                  and (cast(:to as timestamp) is null or wr.createdAt <= :to)
             """)
-    Page<WithdrawalRequest> search(String status, LocalDateTime from, LocalDateTime to, Pageable pageable);
+    Page<WithdrawalRequest> search(
+            @Param("status") String status,
+            @Param("from") LocalDateTime from,
+            @Param("to") LocalDateTime to,
+            Pageable pageable
+    );
+
 }
