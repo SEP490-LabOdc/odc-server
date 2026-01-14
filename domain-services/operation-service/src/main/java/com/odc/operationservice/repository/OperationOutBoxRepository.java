d9318e7d9954c6d528629bf7f07669921da11715
21anhn <ltnanh21@gmail.com>
2026-01-15 00:36:33 +0700

    feat: update EventPublisher, add outbox scheduler for event publishing

--- domain-services/operation-service/src/main/java/com/odc/operationservice/repository/OperationOutBoxRepository.java
+++ domain-services/operation-service/src/main/java/com/odc/operationservice/repository/OperationOutBoxRepository.java
@@ -4,8 +4,11 @@ import com.odc.operationservice.entity.OperationOutBox;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface OperationOutBoxRepository extends JpaRepository<OperationOutBox, UUID> {
+    List<OperationOutBox> findTop50ByProcessedFalseOrderByCreatedAtAsc();
+
 }
--- shared/odc-common-lib/src/main/java/com/odc/commonlib/event/EventPublisher.java
+++ shared/odc-common-lib/src/main/java/com/odc/commonlib/event/EventPublisher.java
@@ -13,8 +13,29 @@ public class EventPublisher {
         this.kafkaTemplate = kafkaTemplate;
     }
 
+    /**
+     * Publish protobuf event
+     */
     public <T extends GeneratedMessageV3> void publish(String topic, T event) {
         byte[] payload = ProtobufConverter.serialize(event);
         kafkaTemplate.send(topic, payload);
+    }
+
+    /**
+     * Publish raw payload (for Outbox Scheduler)
+     */
+    public void publish(String topic, byte[] payload) {
+        kafkaTemplate.send(topic, payload)
+                .whenComplete((result, ex) -> {
+                    if (ex != null) {
+//                        log.error("Failed to publish event to topic={}", topic, ex);
+                    } else {
+//                        log.info(
+//                                "Published event to topic={}, offset={}",
+//                                topic,
+//                                result.getRecordMetadata().offset()
+//                        );
+                    }
+                });
     }
 }
