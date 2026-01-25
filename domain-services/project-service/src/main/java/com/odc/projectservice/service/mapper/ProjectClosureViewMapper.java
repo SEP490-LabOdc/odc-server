8d325ebd4405eb750c73889b3b318cfdc3164728
21anhn <ltnanh21@gmail.com>
2026-01-25 17:41:32 +0700

    update(closure-request): update response model

--- domain-services/project-service/src/main/java/com/odc/projectservice/service/mapper/ProjectClosureViewMapper.java
+++ domain-services/project-service/src/main/java/com/odc/projectservice/service/mapper/ProjectClosureViewMapper.java
@@ -6,12 +6,17 @@ import com.odc.projectservice.dto.response.CompanyClosureView;
 import com.odc.projectservice.dto.response.LabAdminClosureView;
 import com.odc.projectservice.dto.response.MentorClosureView;
 import com.odc.projectservice.entity.ProjectClosureRequest;
+import com.odc.userservice.v1.UserInfo;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class ProjectClosureViewMapper {
 
     public Object toListView(ProjectClosureRequest e, Role role) {
+        return toListView(e, role, null);
+    }
+
+    public Object toListView(ProjectClosureRequest e, Role role, UserInfo userInfo) {
         return switch (role) {
             case MENTOR -> MentorClosureView.builder()
                     .id(e.getId())
@@ -26,16 +31,20 @@ public class ProjectClosureViewMapper {
             case LAB_ADMIN -> LabAdminClosureView.builder()
                     .id(e.getId())
                     .projectId(e.getProject().getId())
-                    .mentorId(e.getRequestedBy())
+                    .projectTitle(e.getProject().getTitle())
+                    .createdBy(e.getRequestedBy())
                     .reason(e.getReason())
                     .summary(e.getSummary())
                     .status(e.getStatus())
                     .createdAt(e.getCreatedAt())
+                    .createdByName(userInfo.getFullName())
+                    .createdByAvatar(userInfo.getAvatarUrl())
                     .build();
 
             case COMPANY -> CompanyClosureView.builder()
                     .id(e.getId())
                     .projectId(e.getProject().getId())
+                    .projectTitle(e.getProject().getTitle())
                     .summary(e.getSummary())
                     .status(e.getStatus())
                     .labAdminComment(e.getLabAdminComment())
